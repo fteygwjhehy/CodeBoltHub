e8688fac455d18a9776ce6d54aa7b9f637962545
fteygwjhehy <nsjcavarroapril@hotmail.com>
2024-05-12 05:46:38 +0700

    update aptos

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-// Setup the client
-const aptosConfig = new AptosConfig({ network: APTOS_NETWORK });
-const aptos = new Aptos(aptosConfig);
+function removeNthFromEnd(head, n) {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let first = dummy;
+  let second = dummy;
+  for (let i = 1; i <= n + 1; i++) {
+    first = first.next;
+  }
+  while (first !== null) {
+    first = first.next;
+    second = second.next;
+  }
+  second.next = second.next.next;
+  return dummy.next;
+}
